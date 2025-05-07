81f04580fc5f5672522662d2798ea6b90235e90e
alimehdizad3 <amehdizade111@gmail.com>
2025-05-07 23:35:48 +0400

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -1,38 +1,42 @@
 import random
-
-lst1 = []
+lst = [ ]
 for i in range(10):
-    number = random.randint(0, 101)
-    lst1.append(number)
+    number = random.randint(0, 101) 
+    lst = lst + [number]
+print("lst = ", lst)
+f = open('file.txt', 'w')
+s=str(len(lst))
+f.write(s + '\n')
+for i in lst:
+    s = str(i) 
+    f.write(s + ' ')
+f.close()
+f=open('file.txt','r')
+s=f.readline()
+lst2=[ ]
+for line in f:
+    strs=line.split()
+    print('strs=',strs)
+    for s in strs:
+        if s!='':
+            lst2=lst2+[int(s)]
+print('lst2=',lst2)
+b=[ ]
+f1 = open('file1.txt', 'w')
+for x in lst2:
+    if x%3==0:
+        b.append(x)
+print('4-e bolunen ededler:',b)
+s=str(len(b))
 
-print("lst1 = ", lst1)
-
-with open('lab7_1.txt', 'w') as f:
-    s = str(len(lst1))
-    f.write(s + '\n')  
-    for i in lst1:
-        s = str(i)
-        f.write(s + ' ')
-
-with open('lab7_1.txt', 'r') as f:
-    s = f.readline()  
-    lst2 = []
-    for line in f:
-        strs = line.split() 
-        for s in strs:
-            if s != '':
-                lst2.append(int(s))  
-
-print('lst2 = ', lst2)
-
-b = [x for x in lst2 if x % 3 == 0]
-print('3-e bolunen ededler:', b)
-
-sum_b = sum(b)
-print('3-e bolunen ededlerin cemi:', sum_b)
-
-with open('lab7_2.txt', 'w') as f:
-    f.write(f"3-e bolunen edeler: {b}\n")
-    f.write(f"Cem: {sum_b}\n")
-    for i in b:
-        f.write(str(i) + ' ')
+f1.write('3-e bolunen edeler: ')
+for i in b:
+    s=str(i)
+    f1.write(s + ' ')
+total=0
+for i in b:
+    total+=int(i)
+f1.write('\n' + '3-e bolunen edelerin cemi: ' + str(total))
+print('3-e bolunen ededlerin cemi:', total)
+f.close()
+f1.close()
