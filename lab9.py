0a6548d4ffddf4493ba170537a75485bf74135d9
alimehdizad3 <amehdizade111@gmail.com>
2025-05-14 21:51:17 +0400

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -1,8 +1,10 @@
 import math
-x0, e = 2.6, 3.0
-x=(x0+1)**(1/3)
+a, b = 2.6, 3
+e=0.0001
+x0=((math.e)**(4*a)+5.5)**(1/3)
+x=((math.e)**(4*x0)+5.5)**(1/3)
 while abs(x-x0)>e:
-    x=(x0+1)**(1/3)
-    x0=x
-    print(x)
+    x0=round(x, 4)
+    x=((math.e)**(4*x0)+5.5)**(1/3)
+    x=round(x, 4)
 print(round(x, 4))
